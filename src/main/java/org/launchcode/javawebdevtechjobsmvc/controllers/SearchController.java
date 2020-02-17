167ffb8662d3b636c44cc21f1e2648a805ed5bc2
Edwin <evb1915@gmail.com>
2020-02-16 19:42:47 -0600

    search funct and view all

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -24,5 +24,25 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    public String displaySearchResults(Model model, @RequestParam String type, @RequestParam String term) {
+        ArrayList<Job> jobs;
+        model.addAttribute("columns", columnChoices);
+        if (!type.toLowerCase().equals("all")) {
+            jobs = JobData.findByColumnAndValue(type, term);
+            model.addAttribute("title", "Job on " + columnChoices.get(type) + ": " + term)
+        }
+        else {
+            if (term.equals("")) {
+                jobs = JobData.findAll();
+                model.addAttribute("title", "Jobs");
+            } else {
+                jobs = JobData.findByColumnAndValue(type, term);
+                model.addAttribute("title", "Jobs on " + term)
+            }
+        }
+        model.addAttribute("jobs", jobs);
+        return "search";
+    }
+
 
 }
